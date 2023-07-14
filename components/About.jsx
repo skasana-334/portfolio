3f797caf7b113e73ac25174a068b4de9a926a47e
Sagar Kasana <93262131+skasana-334@users.noreply.github.com>
2023-07-14 19:35:26 +0530

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -5,19 +5,15 @@ const About = () => {
   return (
     <div id='about me' className='w-full '>
     <div className='max-w-screen-xl mx-auto md:text-left text-center px-4 py-8'>
-   <h2 className='text-5xl md:text-7xl tracking-wider text-green-600 capitalize '>About Me</h2>
+   <h2 className='text-7xl md:text-7xl tracking-wider text-green-600 capitalize  '>About Me</h2>
    <div className='shadow-md shadow-sky-400 my-5 '>
-    <div className='py-4 max-w-2xl mx-auto '>I&apos;m a third year undergratuate pursuing B.tech in information technology from Harcout Butler Technical University.I have a keen interset in problem solving,data structure and algorithms (dsa) and in web developement.
+    <div className='py-4 max-w-2xl mx-auto text-xl'>I&apos;m a Final year undergratuate pursuing B.tech in information technology from Harcout Butler Technical University.<br></br>
+    I am good in problem solving and in DSA(Data Structures & Algorithms).<br></br>
+    I am intersested in Front-End Developement also and i have knowledge of Html,Css and Frameworks of Javascript such as React.js and Tailwind css.
     
     </div>
    </div>
-   <div className='flex justify-center items center gap-x-4  '>
-    <Link href='/Fang sk resume.pdf' download={true}>
-<div className='bg-green-500 p-2 font-semibold uppercase flex justify-center items-center text-white rounded-md px-7 my-6 tracking-wider group cursor-pointer '>Resume
-  <span className='-rotate-90 group-hover:translate-x-3'><MdExpandMore size={25}></MdExpandMore></span>
-  </div>
-  </Link>
-</div>
+
    </div>
    </div>
   )
