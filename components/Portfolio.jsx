5c862fe65f7ada0fa43daa73f88fc81d7cb55399
Sagar Kasana <93262131+skasana-334@users.noreply.github.com>
2023-07-14 19:37:38 +0530

    Update Portfolio.jsx

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -5,29 +5,30 @@ import Image from 'next/image'
 const Portfolio = () => {
   const portfolios=[
     {
-        id:1,
+      id:1,
+      title:'resume Builder',
+    src:'/resumebuild.png',
+    url:'https://resumebuild334.netlify.app/',
+  },
+  {
+    id:2,
+    title:'Videomeet',
+  src:'/meet.png',
+  url:'https://videomeet334.netlify.app/',
+},
+    {
+        id:3,
         title:'Tower Of Hanoi',
       src:'/toh.jpg',
       url:'https://toh334.netlify.app/',
     },
-    {
-        id:2,
-        title:'Dhoondo',
-      src:'/google.jpg',
-      url:'https://github.com/skasana-334/Dhoondo-334',
-    },
-    {
-        id:3,
-        title:'Weather App',
-      src:'/weather.jpg',
-      url:'https://weather334.netlify.app/',
-    },
+ 
   ]
   return (
-    <div id='projects' className='w-full'>
+    <div id='portfolio' className='w-full'>
         <div className='max-w-screen-xl mx-auto text-center md:text-left px-4 py-8'>
        <h2 className='text-5xl md:text-7xl tracking-wider text-green-600 capitalize '>projects</h2>
-       <p className='py-4 max-w-3xl '>These are some of my projects which is built using front end technologies and frameworks such as Reactjs Next.js and Tailwind Css.
+       <p className='py-4 max-w-3xl text-xl '>These are some of my projects which is built using front end technologies and frameworks such as Reactjs Next.js and Tailwind Css.
        <br />
        To explore all my projects click on all projects button.
        </p>
@@ -36,7 +37,7 @@ const Portfolio = () => {
         portfolios.map(({id,title,src,url})=>(
           <Link key={id} href={`${url}`}>
             <div className='cursor-pointer   group shadow-md shadow-sky-600 overflow-hidden rounded-md '>
-              <Image src={src} alt={title} width='900' height='400' className='rounded-md duration-200 hover:scale-110' ></Image>
+              <Image src={src} alt={title} width='900' height='400' className='rounded-md duration-200 hover:scale-110 ' ></Image>
               <h2 className='text-center text-base capitalize  group-hover:underline underline-offset-4 duration-200 my-2 text-violet-500'>{title}</h2>
               
             </div>
@@ -46,7 +47,7 @@ const Portfolio = () => {
       }
      <div className='flex justify-center items-center'>
      <Link href='/portfolio'>
-  <div className='bg-green-500 p-2 font-semibold uppercase flex justify-center items-center text-white rounded-md px-3 my-6 tracking-wider group cursor-pointer'>all projects
+  <div className='bg-green-500 p-2 font-semibold uppercase flex justify-center items-center text-white rounded-md px-3 my-6 tracking-wider group cursor-pointer'>All Projects
   <span className='-rotate-90 group-hover:translate-x-3'><MdExpandMore size={25}></MdExpandMore></span>
   </div>
   
