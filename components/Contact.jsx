2c640f825934cb74dc3053e4300053f4eb46e308
Sagar Kasana <93262131+skasana-334@users.noreply.github.com>
2023-07-14 19:35:59 +0530

    Update Contact.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
-import contactimg from '../public/exp/contactimg.jpg'
-import { FaBars, FaEnvelopeSquare,  FaGithub,  FaInstagram,  FaLinkedin, FaTimes } from 'react-icons/fa'
+import contact from '../public/exp/contact.jpg'
+import { FaGithub,  FaLinkedin } from 'react-icons/fa'
 import {IoMdCall} from 'react-icons/io'
 import {SiGmail} from 'react-icons/si'
 import Link from 'next/link'
@@ -12,16 +12,21 @@ const Contact = () => {
        <h2 className='text-5xl md:text-7xl tracking-wider text-green-600 capitalize '>contact me</h2>
        <div className='flex flex-col md:flex-row gap-8 mt-4 shadow-lg shadow-blue-400'>
         <div className='w-full md:w-1/2 h-full rounded-xl p-4 mt-4 flex flex-col'>
-            <Image src={contactimg}  alt='no pic' width='450' height='300 ' className='rounded-2xl'/>
-            <p className='text-2xl pt-2 pb-3'>I am open to talk regarding internships or full-time opportunities.Feel free to contact me using your preferred medium.</p>
+            <Image src={contact}  alt='no pic' width='450' height='300 ' className='rounded-2xl'/>
+            <p className='text-2xl pt-2 pb-3'>I am open to talk regarding internships or full-time opportunities.
+            <div>
+            Feel free to contact me using your preferred medium.</div></p>
           
        </div>
        
      
  
-      <div className='my-auto mx-auto'>
-<Link href='https://www.truecaller.com/' >
-<IoMdCall size={25} className='ease-in duration-75 hover:scale-125 cursor-pointer'></IoMdCall></Link>
+      <div className='my-auto mx-auto '>
+      <Link href="tel:+919999572480">
+      <a>
+        <IoMdCall size={25} className='ease-in duration-75 hover:scale-125 cursor-pointer inline' />
+      </a>
+    </Link>
 <h1 className=' px-8 text-xl'>9999572480</h1>
 <button onClick={() => window.location = 'mailto:sagarkasana482@gmail.com'} className='ease-in duration-75 hover:scale-125'>
 <SiGmail size={25}></SiGmail></button>
