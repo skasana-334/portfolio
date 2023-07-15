e953e236c5f6bb082a6c6d6b75d9f34ecb3917a7
Sagar Kasana <93262131+skasana-334@users.noreply.github.com>
2023-07-15 20:39:31 +0530

    Update Homepage.jsx

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -12,7 +12,7 @@ const Homepage = () => {
 <h1 className='text-7xl text-gray-500 '>“Hi <span className='text-cyan-600'>there!</span>”</h1>
 <p className='max-w-sm text-xl text-center t'>“Welcome to my portfolio! I am an engineering student with a strong understanding of Data Structures and Algorithms and  interested in front-end development. Please take a look at my work to see what I am capable of.”</p>
 <div className='flex justify-center items center gap-x-4  '>
-    <Link href='/skresume.pdf' download={true}>
+    <Link href='/myresume.pdf' download={true}>
 <div className='bg-green-500 p-2 font-semibold uppercase flex justify-center items-center text-white rounded-md px-7 my-6 tracking-wider group cursor-pointer '>Resume
   <span className='-rotate-90 group-hover:translate-x-3'><MdExpandMore size={25}></MdExpandMore></span>
   </div>
