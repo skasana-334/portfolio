231bc5edf42a106acde734c6314065194f01569e
Sagar Kasana <93262131+skasana-334@users.noreply.github.com>
2024-10-19 12:49:26 +0530

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
     <div className='max-w-screen-xl mx-auto md:text-left text-center px-4 py-8'>
    <h2 className='text-7xl md:text-7xl tracking-wider text-green-600 capitalize  '>About Me</h2>
    <div className='shadow-md shadow-sky-400 my-5 '>
-    <div className='py-4 max-w-2xl mx-auto text-xl'>I&apos;m a Final year undergraduate pursuing B.Tech in Information Technology from Harcourt Butler Technical University.<br></br>
+    <div className='py-4 max-w-2xl mx-auto text-xl'>I&apos;m a Software Engineer and i have completed my B.Tech in Information Technology from Harcourt Butler Technical University.<br></br>
     I am good in problem solving and in DSA(Data Structures & Algorithms) and excel in problem-solving.<br></br>
     I am interested in Front-End Development also and i have knowledge of Html,Css and Frameworks of Javascript such as React.js and Tailwind css.
     
