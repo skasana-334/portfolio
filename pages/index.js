a3ca9825ab16afe5a315fac6d5bed777c42f0707
Sagar Kasana <93262131+skasana-334@users.noreply.github.com>
2025-08-27 14:53:18 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import About from '../components/About'
-
+import Photo from '../components/Photo'
 import Experience from '../components/Experience'
 import Homepage from '../components/Homepage'
 import Portfolio from '../components/Portfolio'
 import Coding from '../components/Coding'
 import React, { useEffect, useState } from 'react'
 import Contact from '../components/Contact'
+import Skills from '../components/Skills'
 
 export default function Home() {
   const[pgscroll,setpgscroll]=useState(false);
@@ -21,9 +22,10 @@ useEffect(()=>{
    <div className={`${pgscroll&&'bg-gradient-to-b from-gray-800 to-black text-[#bfbed8]'}`}>
     <Homepage></Homepage>
     <About/>
+     <Experience/>
    <Portfolio />
    <Coding></Coding>
-   <Experience/>
+   <Skills/>
   <Contact/>
    </div>
   )
